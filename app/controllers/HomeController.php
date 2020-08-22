ab7eff0318bdb396dfcf20351b54edd004e0d9ac
paster0 <paster01@gmail.com>
2020-08-22 13:57:23 +0200

    no message

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -18,6 +18,70 @@ class HomeController extends BaseController {
 
 	public function showWelcome()
 	{
+
+
+
+
+
+
+        $file = fopen('demosaved.csv', 'w');
+ 
+        // save the column headers
+        fputcsv($file, array('Product ID', 'Active (0/1)', 'Name', 'Categories (x,y,z...)', 'Price tax included'
+    ,'Tax rules ID', 'Wholesale price','On sale (0/1)','Discount amount','Discount percent'
+,'Discount from (yyyy-mm-dd)','Discount to (yyyy-mm-dd)','Reference #', 'Supplier reference #', 'Supplier', 'Manufacturer'
+,'EAN13', 'UPC', 'Ecotax', 'Width', 'Height', 'Depth', 'Weight', 'Delivery time of in-stock products', 
+'Delivery time of out-of-stock products with allowed orders','Quantity', 'Minimal quantity', 'Low stock level'
+,'Send me an email when the quantity is under this level', 'Visibility', 'Additional shipping cost'
+,'Unity', 'Unit price', 'Summary', 'Description', 'Tags (x,y,z...)', 'Meta title', 'Meta keywords', 'Meta description'
+,'URL rewritten', 'Text when in stock', 'Text when backorder allowed', 'Available for order (0 = No, 1 = Yes)','Product available date'
+,'Product creation date', 'Show price (0 = No, 1 = Yes)', 'Image URLs (x,y,z...)','Image alt texts (x,y,z...)', 'Delete existing images (0 = No, 1 = Yes)'
+,'Feature(Name:Value:Position)', 'Available online only (0 = No, 1 = Yes)', 'Condition', 'Customizable (0 = No, 1 = Yes)', 'Uploadable files (0 = No, 1 = Yes)'
+,'Text fields (0 = No, 1 = Yes)', 'Out of stock action', 'Virtual product', 'File URL', 'Number of allowed downloads', 'Expiration date'
+,'Number of days', 'Number of days', 'Advanced stock management', 'Depends On Stock', 'Warehouse', 'Acessories  (x,y,z...)'));
+         
+        // Sample data. This can be fetched from mysql too
+        $data = array(
+        array('Data 11', 'Data 12', 'Data 13', 'Data 14', 'Data 15'),
+        array('Data 21', 'Data 22', 'Data 23', 'Data 24', 'Data 25'),
+        array('Data 31', 'Data 32', 'Data 33', 'Data 34', 'Data 35'),
+        array('Data 41', 'Data 42', 'Data 43', 'Data 44', 'Data 45'),
+        array('Data 51', 'Data 52', 'Data 53', 'Data 54', 'Data 55')
+        );
+         
+        // save each row of the data
+        foreach ($data as $row)
+        {
+        fputcsv($file, $row);
+        }
+         
+        // Close the file
+        fclose($file);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $token = $this->getToken();
 $resonse = $this->getMainProducts($token);
 $obj = json_decode($resonse, true);
