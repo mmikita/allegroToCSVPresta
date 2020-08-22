725f6d2a866c0622796f16d5081f9d5b200aeb22
paster0 <paster01@gmail.com>
2020-08-22 11:17:43 +0200

    no message

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -19,8 +19,11 @@ class HomeController extends BaseController {
 	public function showWelcome()
 	{
 $token = $this->getToken();
-$cats =  $this->getMainCategories($token);
-print_r($cats);
+
+
+
+
+print_r($this->getMainProducts($token));
 
   //     return View::make('hello');
 
@@ -57,54 +60,30 @@ function getToken(){
 }
 
 
-	function getMainCategories(String $token)
+	function getMainProducts(String $token)
     {
-        $getCategoriesUrl = "https://api.allegro.pl.allegrosandbox.pl/sale/categories";
-    
-        $ch = curl_init($getCategoriesUrl);
-    
+        $opinie = "https://api.allegro.pl.allegrosandbox.pl/offers/listing?seller.id=44292194";
+
+        $ch = curl_init($opinie);
+        
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
                      "Authorization: Bearer $token",
                      "Accept: application/vnd.allegro.public.v1+json"
         ]);
-    
+        
         $mainCategoriesResult = curl_exec($ch);
         $resultCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
-    
+        
         if ($mainCategoriesResult === false || $resultCode !== 200) {
             exit ("Something went wrong");
         }
-    
+        
         $categoriesList = json_decode($mainCategoriesResult);
-
-
-        $file = fopen('demosaved.csv', 'w');
- 
-        // save the column headers
-        fputcsv($file, array('Column 1', 'Column 2', 'Column 3', 'Column 4', 'Column 5'));
-         
-        // Sample data. This can be fetched from mysql too
-        $data = array(
-        array('Data 11', 'Data 12', 'Data 13', 'Data 14', 'Data 15'),
-        array('Data 21', 'Data 22', 'Data 23', 'Data 24', 'Data 25'),
-        array('Data 31', 'Data 32', 'Data 33', 'Data 34', 'Data 35'),
-        array('Data 41', 'Data 42', 'Data 43', 'Data 44', 'Data 45'),
-        array('Data 51', 'Data 52', 'Data 53', 'Data 54', 'Data 55')
-        );
-         
-        // save each row of the data
-        foreach ($data as $row)
-        {
-        fputcsv($file, $row);
-        }
-         
-        // Close the file
-        fclose($file);
-
-    
-        return $categoriesList;
+        
+        
+        return json_encode($categoriesList, JSON_PRETTY_PRINT);
 	}
 	
 
